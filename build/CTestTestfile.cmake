ff12389236431737dd9fceae6b7449847c8a749a
wwdu <wwdu@nullmax.ai>
2018-08-14 18:39:41 +0800

    add Sentor Segment

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/halo/WorkSpace/Columbus_Ground_Detection/src
-# Build directory: /home/halo/WorkSpace/Columbus_Ground_Detection/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(rviz_cloud_annotation)
